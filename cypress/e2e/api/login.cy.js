d2dcfe360852f9513db821807418ff3a2d0393b1
Vitoria Regina <vitoriasantos78690@gmail.com>
2024-01-23 19:55:51 -0300

    usando fixture

--- cypress/e2e/api/login.cy.js
+++ cypress/e2e/api/login.cy.js
@@ -1,6 +1,16 @@
 /// <reference types="Cypress" />
 
 describe('Deve realizar login', () => {
+    let data; 
+
+    before(() => {
+        cy.fixture('example').then((tData) => {
+            data = tData;
+            cy.log(data.username)
+            cy.log(data.password)
+        })
+    });
+
     it('Realizar login atraves da api da aplicaçao', () => {
         cy.request({
             method:'POST',
@@ -10,8 +20,8 @@ describe('Deve realizar login', () => {
             },
 
             body: {
-                username:"admin@automacao.org.br",
-                password: "password01"
+                username: data.username,
+                password: data.password
             }
         }).then((response) => {
             expect(response.status).to.eq(200)
--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,10 +1,18 @@
 /// <reference types="cypress"/>
 
+    let data; 
+
+    before(() => {
+        cy.fixture('example').then((tData) => {
+            data = tData;
+        })
+    });
+
 describe('Acessar a página de login', () => {
     it('Deve fazer login com sucesso', () => {
         cy.visit('https://curso-automacao-web-app.herokuapp.com/login')
-        cy.get('#yourUsername').type("admin@automacao.org.br")
-        cy.get('#yourPassword').type("password01")
+        cy.get('#yourUsername').type(data.username)
+        cy.get('#yourPassword').type(data.password)
         cy.get('#logginButton').click()
     });
 });
